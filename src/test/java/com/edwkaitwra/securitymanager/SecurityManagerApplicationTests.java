bbab5055a6a9895ade80456d464c872cfab0f29e
John Triantafyllakis <john.3fyl@gmail.com>
2022-04-25 02:26:23 +0300

    Update SecurityManagerApplicationTests.java
    Removed Tests (For now :-1:  )

--- src/test/java/com/edwkaitwra/securitymanager/SecurityManagerApplicationTests.java
+++ src/test/java/com/edwkaitwra/securitymanager/SecurityManagerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SecurityManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
