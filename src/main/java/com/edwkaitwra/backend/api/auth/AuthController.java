ba5f3718fd79c151910ee46759c827cc048a3e2e
John Triantafyllakis <john.3fyl@gmail.com>
2022-04-25 22:45:11 +0300

    Created Auth controller
    Created Auth controller

---
+++ src/main/java/com/edwkaitwra/backend/api/auth/AuthController.java
@@ -0,0 +1,90 @@
+package com.edwkaitwra.backend.api.auth;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.edwkaitwra.backend.domain.Role;
+import com.edwkaitwra.backend.domain.User;
+import com.edwkaitwra.backend.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Slf4j
+public class AuthController {
+    @Autowired
+    UserService userService;
+
+    @Value("${key.access-token-expired}")
+    private Integer accessTokenExpiredInDays;
+
+    @Value("${key.jwt-secret}")
+    private String jwtSecret;
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        log.info("Rest request to refresh token");
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String refresh_token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256(jwtSecret.getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refresh_token);
+                String username = decodedJWT.getSubject();
+                User user = userService.getUser(username);
+                String access_token = JWT.create()
+                        .withSubject(user.getEmail())
+                        .withExpiresAt(currentDatePlusDay(accessTokenExpiredInDays))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", user.getRole().stream().map(Role::getName).collect(Collectors.toList()))
+                        .sign(algorithm);
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", access_token);
+                tokens.put("refresh_token", refresh_token);
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+
+            } catch (Exception e) {
+                response.setStatus(FORBIDDEN.value());
+                Map<String, String> error = new HashMap<>();
+                log.error(e.getMessage());
+                error.put("error", "Generic Error");
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        } else {
+            log.error("Refresh Token is missing");
+            throw new RuntimeException("Refresh Token is missing");
+        }
+    }
+
+    static Date currentDatePlusDay(int days) {
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DATE, days);
+        Date date = c.getTime();
+        return date;
+    }
+}
---
+++ src/main/java/com/edwkaitwra/backend/dto/RoleToUserForm.java
@@ -0,0 +1,9 @@
+package com.edwkaitwra.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class RoleToUserForm {
+    private String email;
+    private String roleName;
+}
