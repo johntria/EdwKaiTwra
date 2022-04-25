b867a1a66285132a8aa19c2d52ab67f5a5389e84
John Triantafyllakis <john.3fyl@gmail.com>
2022-04-25 22:45:29 +0300

    Created Auth COntroller

--- src/main/java/com/edwkaitwra/backend/api/UserController.java
+++ src/main/java/com/edwkaitwra/backend/api/UserController.java
@@ -1,31 +1,18 @@
 package com.edwkaitwra.backend.api;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.edwkaitwra.backend.domain.Role;
 import com.edwkaitwra.backend.domain.User;
+import com.edwkaitwra.backend.dto.RoleToUserForm;
 import com.edwkaitwra.backend.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.annotation.security.RolesAllowed;
 import java.net.URI;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -34,10 +21,9 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class UserController {
     private final UserService userService;
 
-    @Value("${key.access-token-expired}")
-    private Integer accessTokenExpiredInDays;
 
     @GetMapping("/users")
+    @RolesAllowed({"GODUSER"})
     public ResponseEntity<List<User>> getUser() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
@@ -60,55 +46,6 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
-    static Date currentDatePlusDay(int days) {
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DATE, days);
-        Date date = c.getTime();
-        return date;
-    }
-
-    @GetMapping("/token/refresh")
-    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        UserController.log.info("Rest request to refresh token");
-        String authorizationHeader = request.getHeader(AUTHORIZATION);
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            try {
-                String refresh_token = authorizationHeader.substring("Bearer ".length());
-                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-                JWTVerifier verifier = JWT.require(algorithm).build();
-                DecodedJWT decodedJWT = verifier.verify(refresh_token);
-                String username = decodedJWT.getSubject();
-                User user = userService.getUser(username);
-                String access_token = JWT.create()
-                        .withSubject(user.getEmail())
-                        .withExpiresAt(currentDatePlusDay(accessTokenExpiredInDays))
-                        .withIssuer(request.getRequestURL().toString())
-                        .withClaim("roles", user.getRole().stream().map(Role::getName).collect(Collectors.toList()))
-                        .sign(algorithm);
-                Map<String, String> tokens = new HashMap<>();
-                tokens.put("access_token", access_token);
-                tokens.put("refresh_token", refresh_token);
-                response.setContentType(APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
-
-            } catch (Exception e) {
-                response.setStatus(FORBIDDEN.value());
-                Map<String, String> error = new HashMap<>();
-                UserController.log.error(e.getMessage());
-                error.put("error", "Generic Error");
-                response.setContentType(APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), error);
-            }
-        } else {
-            UserController.log.error("Refresh Token is missing");
-            throw new RuntimeException("Refresh Token is missing");
-        }
-    }
 
 }
 
-@Data
-class RoleToUserForm {
-    private String email;
-    private String roleName;
-}
