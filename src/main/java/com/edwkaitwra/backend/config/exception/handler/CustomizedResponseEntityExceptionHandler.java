7312d45e20e4e21137cf577a09872e999ea69acf
John Triantafyllakis <john.3fyl@gmail.com>
2022-05-10 10:12:33 +0300

    Improved  Response Entity Exception because we have to handle all collection in one method

--- src/main/java/com/edwkaitwra/backend/config/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/edwkaitwra/backend/config/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.edwkaitwra.backend.config.exception.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -13,11 +16,22 @@ import java.time.LocalDateTime;
 
 @ControllerAdvice
 @Slf4j
-public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler implements ErrorController {
+
+
+    @ExceptionHandler(UserIsNotActivatedException.class)
+    public final ResponseEntity<?> userIsNotActivated(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), HttpStatus.FORBIDDEN, ex.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
+    }
+
     @ExceptionHandler(Exception.class)
+    @RequestMapping("/error")
     public final ResponseEntity<?> handleAllExceptions(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
-        return new ResponseEntity<>(exceptionResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 
 }
