8c14476fbed9acc6241b1f0719061538c26b1535
John Triantafyllakis <john.3fyl@gmail.com>
2022-05-10 10:09:08 +0300

    EnableAutoConfiguration Removed due to is included in SpringBootApplication

--- src/main/java/com/edwkaitwra/backend/BackEndApplication.java
+++ src/main/java/com/edwkaitwra/backend/BackEndApplication.java
@@ -1,14 +1,12 @@
 package com.edwkaitwra.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class BackEndApplication {
 
     public static void main(String[] args) {
