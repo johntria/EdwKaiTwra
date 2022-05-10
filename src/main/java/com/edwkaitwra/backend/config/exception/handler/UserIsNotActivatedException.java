593a32fa4868bae3456ab6304a4e4678b762ec9e
John Triantafyllakis <john.3fyl@gmail.com>
2022-05-10 10:04:28 +0300

    Implemented Exception from UserIsNotActivated

---
+++ src/main/java/com/edwkaitwra/backend/config/exception/handler/UserIsNotActivatedException.java
@@ -0,0 +1,23 @@
+package com.edwkaitwra.backend.config.exception.handler;
+
+public class UserIsNotActivatedException extends RuntimeException {
+
+    public UserIsNotActivatedException() {
+    }
+
+    public UserIsNotActivatedException(String message) {
+        super(message);
+    }
+
+    public UserIsNotActivatedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserIsNotActivatedException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserIsNotActivatedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
