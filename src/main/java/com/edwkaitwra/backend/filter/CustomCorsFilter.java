197e0f22685df2dcdaa13b2ff0d1ed728ff8c126
John Triantafyllakis <john.3fyl@gmail.com>
2022-07-04 08:16:04 +0300

    Preflight header created

--- src/main/java/com/edwkaitwra/backend/filter/CustomCorsFilter.java
+++ src/main/java/com/edwkaitwra/backend/filter/CustomCorsFilter.java
@@ -12,8 +12,8 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
-import static org.springframework.http.HttpHeaders.ORIGIN;
+import static org.springframework.http.HttpHeaders.REFERER;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -25,13 +25,24 @@ public class CustomCorsFilter implements Filter {
         this.allowedCors = allowedCors;
     }
 
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 
-        String corsHeader = httpServletRequest.getHeader(ORIGIN);
+        String corsHeader = httpServletRequest.getHeader(REFERER);
+        httpResponse.setHeader("Access-Control-Allow-Origin", allowedCors);
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        httpResponse.addHeader("Access-Control-Expose-Headers", "xsrf-token");
         try {
+            if ("OPTIONS".equalsIgnoreCase(httpServletRequest.getMethod())) {
+                httpResponse.setStatus(OK.value());
+                return;
+            }
+            
             if (corsHeader == null || corsHeader.trim().isEmpty()) {
                 log.error("Cors is empty or blank");
                 throw new CorsNotExistsException("Cors Not Exists");
@@ -40,8 +51,10 @@ public class CustomCorsFilter implements Filter {
                 log.error("Cors not contains origin");
                 throw new CorsNotExistsException("Cors Not Exists");
             }
-            httpResponse.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, allowedCors);
+
+
             filterChain.doFilter(request, response);
+
         } catch (RuntimeException runtimeException) {
             httpResponse.setStatus(UNAUTHORIZED.value());
             Map<String, String> error = new HashMap<>();
